012b84087321ea84c912ce05610b2d351d890810
dev-jino <wlsgh2029@naver.com>
2024-01-09 18:49:25 +0900

    240109

---
+++ src/inflearn/section1_String/_1_문자찾기.java
@@ -0,0 +1,33 @@
+package inflearn.section1_String;
+
+import java.util.Scanner;
+
+public class _1_문자찾기 {
+    public int solution(String str, char t) {
+        int answer = 0;
+        str = str.toLowerCase();
+        t = Character.toLowerCase(t);
+
+//        for (int i = 0 ; i < str.length() ; i++) {
+//            if (str.charAt(i) == t) {
+//                answer++;
+//            }
+//        }
+
+        for (char x : str.toCharArray()) {
+            if (x == t) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        _1_문자찾기 T = new _1_문자찾기();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        char c = sc.next().charAt(0);
+        System.out.println(T.solution(str, c));
+    }
+}
