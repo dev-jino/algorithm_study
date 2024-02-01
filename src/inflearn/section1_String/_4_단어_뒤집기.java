99ae30ab431cdfd82e38a6f5d53da38cd88dba96
dev-jino <wlsgh2029@naver.com>
2024-02-01 23:10:50 +0900

    240201

--- src/inflearn/section1_String/_4_단어_뒤집기.java
+++ src/inflearn/section1_String/_4_단어_뒤집기.java
@@ -1,6 +1,54 @@
 package inflearn.section1_String;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class _4_단어_뒤집기 {
+    public ArrayList<String> solution(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+
+        for (String s : str) {
+            String tmp = new StringBuilder(s).reverse().toString();
+            answer.add(tmp);
+        }
+
+        return answer;
+    }
+
+    public ArrayList<String> solution_2(int n, String[] str) {
+        ArrayList<String> answer = new ArrayList<>();
+
+        for (String s : str) {
+            char[] c = s.toCharArray();
+            int lt = 0;
+            int rt = s.length() - 1;
+
+            while (lt < rt) {
+                char tmp = c[lt];
+                c[lt] = c[rt];
+                c[rt] = tmp;
+                lt++;
+                rt--;
+            }
+            String tmp = String.valueOf(c);
+            answer.add(tmp);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        _4_단어_뒤집기 T = new _4_단어_뒤집기();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] str = new String[n];
+        for (int i = 0; i < n; i++) {
+            str[i] = sc.next();
+        }
+
+        for (String s : T.solution_2(n, str)) {
+            System.out.println(s);
+        }
+
+    }
 }
